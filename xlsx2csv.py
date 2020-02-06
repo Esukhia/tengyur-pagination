f511efd3985b6bcd274252e58003ebbb23e27dab
ngawangtrinley <ngawangtrinley@gmail.com>
2020-02-07 03:17:18 +0800

    comments

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -7,22 +7,26 @@ sourceDir = 'sources'
 
 print(f'finding xlsx files in {sourceDir}')
 
+# get all excel files in sources
 xlFiles = glob.glob(f'{sourceDir}/**/*.xlsx', recursive=True)
 
 def xlsx2csv(xlFile):
+    # convert to csv with parent dir
     xlFile = Path(xlFile)
 
+    # get data from excel file
     xlsx = openpyxl.load_workbook(xlFile, data_only=True)
     sheet = xlsx.active
     data = sheet.rows
 
+    # create/open dir and create csv
     outDir = Path.cwd() / xlFile.parts[-2].split(' ')[0]    # strip the tibetan with split
     outDir.mkdir(parents=True, exist_ok=True)
     outStem = xlFile.stem
     outPath = outDir / f'{outStem}.csv'
 
+    # write data in csv
     csv = open(outPath, "w+", encoding='utf-8')
-
     for row in data:
         l = list(row)
         for i in range(len(l)):
@@ -32,6 +36,7 @@ def xlsx2csv(xlFile):
                 csv.write(str(l[i].value) + ',')
         csv.write('\n')
     csv.close()
+
 
 for xl in xlFiles:
     print(f'converting: {xl}')
