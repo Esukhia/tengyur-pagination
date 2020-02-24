8be9f59520c6ac2bb3ee346d31377355eef1e826
ngawangtrinley <ngawangtrinley@gmail.com>
2020-02-24 20:53:36 +0800

    final

---
+++ pagination.py
@@ -0,0 +1,4 @@
+# map bdrc volume names "dbu ma, gi" or "mdo 'grel (dbu ma), tsha" to Image group numbers
+# create a table per work with [volume name, 1a/1b, གཅིག་གཉིས་, image url]
+# add this data to: https://docs.google.com/spreadsheets/d/1ikSWRy7MdzFXs_0e33wa6Hi2-mb32rNLPeAZOrdYwtk/edit#gid=0
+# 
